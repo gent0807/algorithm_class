1ccf27885e7642073b3ae57266c2c7337e059fef
gent0807 <theloopholesnk@gmail.com>
2022-05-23 11:14:22 +0900

    ddd

--- 이진검색트리.c
+++ 이진검색트리.c
@@ -13,8 +13,7 @@ void main() {
 	char wbuf[30];
 
 	printf(" 검색 트리에 저장할 단어를 입력하세요.\n 입력의 끝에는 quit를 입력하세요.\n");
-
-	while(strcmp(gets(wbuf), "quit")) {
+	while (strcmp(gets(wbuf), "quit")) {
 		if (!tree) {
 			tree = (NODETYPE*)malloc(sizeof(NODETYPE));
 			tree->word = (char*)malloc(strlen(wbuf) + 1);
@@ -23,10 +22,7 @@ void main() {
 		}
 		else
 			insert_node(tree, wbuf);
-
-
 	}
-
 	printf("\n\nEnter a key to search: ");
 	gets(wbuf);
 	ptr = search(tree, wbuf);
@@ -42,6 +38,46 @@ NODETYPE* search(NODETYPE* root, char* key) {
 	int cmp;
 	while (tptr) {
 		cmp = strcmp(key, tptr->word);
-		if()
+		if (cmp < 0)
+			tptr = tptr->left;
+		else if (cmp > 0)
+			tptr = tptr->right;
+		else
+			return tptr;
+	}
+	return NULL;
+}
+int insert_node(NODETYPE* root, char* word) {
+	NODETYPE* tptr = root, * before = NULL;
+	int cmp;
+	while (tptr) {
+		cmp = strcmp(word, tptr->word);
+		if (cmp < 0) {
+			before = tptr;
+			tptr = tptr->left;
+		}
+		else if (cmp > 0) {
+			before = tptr;
+			tptr = tptr->right;
+		}
+		else
+			return 0;
+	}
+
+	tptr = (NODETYPE*)malloc(sizeof(NODETYPE));
+	tptr->word = (char*)malloc(strlen(word) + 1);
+	strcpy(tptr->word, word);
+	tptr->left = tptr->right = NULL;
+	if (cmp < 0) before->left = tptr;
+	else before->right = tptr;
+	return 1;
+
+}
+void inorder(NODETYPE* ptr) {
+
+	if (ptr) {
+		inorder(ptr->left);
+		printf("%s\n", ptr->word);
+		inorder(ptr->right);
 	}
 }
